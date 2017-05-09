77b3b22ef3caa8e60dbb98773044216385e732d1
Joey Ciechanowicz <joey.ciechanowicz@macmillan.com>
2017-05-09 13:19:54 +0100

    Removed multiple replaces in favour of a single replace with a match function that does a lookup. Avoids multiple iterations of the input

--- index.js
+++ index.js
@@ -2,25 +2,31 @@
  * node-s3-url-encode - Because s3 urls are annoying
  */
 
+var encodings = {
+  '\+': "%2B",
+  '\!': "%21",
+  '\"': "%22",
+  '\#': "%23",
+  '\$': "%24",
+  '\&': "%26",
+  '\'': "%27",
+  '\(': "%28",
+  '\)': "%29",
+  '\*': "%2A",
+  '\,': "%2C",
+  '\:': "%3A",
+  '\;': "%3B",
+  '\=': "%3D",
+  '\?': "%3F",
+  '\@': "%40",
+};
+
 function encodeS3URI(filename) {
   return encodeURI(filename) // Do the standard url encoding
-              .replace(/\+/img, "%2B")
-              .replace(/\!/img, "%21")
-              .replace(/\"/img, "%22")
-              .replace(/\#/img, "%23")
-              .replace(/\$/img, "%24")
-              .replace(/\&/img, "%26")
-              .replace(/\'/img, "%27")
-              .replace(/\(/img, "%28")
-              .replace(/\)/img, "%29")
-              .replace(/\*/img, "%2A")
-              .replace(/\+/img, "%2B")
-              .replace(/\,/img, "%2C")
-              .replace(/\:/img, "%3A")
-              .replace(/\;/img, "%3B")
-              .replace(/\=/img, "%3D")
-              .replace(/\?/img, "%3F")
-              .replace(/\@/img, "%40");
+              .replace(
+                  /(\+|!|"|#|\$|&|'|\(|\)|\*|\+|,|:|;|=|\?|@)/img,
+                  function(match) { return encodings[match]; }
+              );
 
 }
 
